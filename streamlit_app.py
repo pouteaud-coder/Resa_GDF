1eb4c0f30482e8c3dea41fbc346fd31ccc913fb2
pouteaud-coder <pouteaud@gmail.com>
2026-04-08 19:21:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,7 +66,7 @@ st.markdown("""
     .animateur-inscrit { font-size: 0.95rem !important; color: #e65100; font-weight: bold; margin-left: 20px; display: block; line-height: 1.1; }
     .animateur-badge { background-color: #e65100; color: white; padding: 1px 7px; border-radius: 4px; font-size: 0.78rem; font-weight: bold; margin-left: 5px; }
     .nb-enfants-focus { color: #0a3d0a; font-weight: 600; }
-    .stButton button { border-radius: 12px !important; background-color: #b2d8d8 !important; color: white !important; border: 0.5px solid #b2d8d8 !important; }
+    .stButton button { border-radius: 8px !important; background-color: #b2d8d8 !important; color: white !important; border: 0.2px solid #b2d8d8 !important; }
     .stButton button:hover { background-color: #0a3d0a !important; }
     .badge-verrouille { background-color: #b2d8d8; color: white; padding: 2px 8px; border-radius: 4px; font-size: 0.8rem; font-weight: bold; margin-left: 6px; }
     .stTabs [data-baseweb="tab-list"] button [data-testid="stMarkdownContainer"] p { color: #1b5e20 !important; }
